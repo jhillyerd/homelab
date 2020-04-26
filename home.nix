46f77766607be3a3fb6bb91c9d49888efe1faea3
James Hillyerd <james@hillyerd.com>
2020-04-26 15:54:36 -0700

    webserver: init empty influxdb + grafana config

--- home.nix
+++ home.nix
@@ -1,3 +1,12 @@
+let
+  grafanaPort = 3000;
+  buildGrafanaInfluxSource = db: {
+    name = "Influx-${db} DB";
+    type = "influxdb";
+    database = db;
+    url = "http://127.0.0.1:8086";
+  };
+in
 {
   network.description = "Home Services";
 
@@ -7,11 +16,24 @@
       mypkgs = import ./pkgs { nixpkgs = pkgs; };
     in with mypkgs;
     {
+      services.grafana = {
+        enable = true;
+        addr = "";
+        port = grafanaPort;
+        domain = "webserver.skynet.local";
+        provision = {
+          enable = true;
+          datasources = map buildGrafanaInfluxSource [ "home" ];
+        };
+      };
+
+      services.influxdb.enable = true;
+
       services.nginx.enable = true;
       services.nginx.virtualHosts."127.0.0.1" = {
         root = "${website}";
       };
 
-      networking.firewall.allowedTCPPorts = [ 80 ];
+      networking.firewall.allowedTCPPorts = [ 80 grafanaPort ];
     };
 }
