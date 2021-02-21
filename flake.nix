923592394e2cab4167a38c8a644410daa8d8dcbf
James Hillyerd <james@hillyerd.com>
2021-02-21 12:11:44 -0800

    add latest nixops w/ all plugins flake

--- flake.nix
+++ flake.nix
@@ -4,9 +4,10 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     hello.url = "github:ihsanturk/hello-world-nix?rev=03ec3abd2def85a97425660a96aa31565cc77821"; 
+    nixops-flake.url = "github:input-output-hk/nixops-flake";
   };
 
-  outputs = { self, nixpkgs, flake-utils, hello }:
+  outputs = { self, nixpkgs, flake-utils, hello, nixops-flake }:
     flake-utils.lib.eachDefaultSystem
       (system: let
         pkgs = nixpkgs.legacyPackages.${system};
@@ -16,6 +17,7 @@
             buildInputs = with pkgs; [
               ansible
               hello.defaultPackage.${system}
+              nixops-flake.defaultPackage.${system}
             ];
           };
         }
