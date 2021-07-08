433eb19bb16eef0a0956b61c8ae11d98e0d2e20e
James Hillyerd <james@hillyerd.com>
2021-07-07 21:29:14 -0700

    nexus: migrate from VM to MSI Cubi hardware

--- env-prod.nix
+++ env-prod.nix
@@ -1,9 +1,13 @@
+# NOTE:
+#   You must run `nixops set-args --arg-str environment prod` before the
+#   initial deploy of the prod environment.
+
 let
   hw = import ./hw;
 in
 {
-  nexus = hw.kvmGuest {
+  nexus = hw.msiCubi {
     name = "nexus";
-    ip = "192.168.1.11";
+    ip = "192.168.1.10";
   };
 }
--- hw/default.nix
+++ hw/default.nix
@@ -1,5 +1,34 @@
 # Hardware templates
 {
+  # MSI Cubi Mini PC
+  msiCubi =
+    { name, ip }:
+    { config, pkgs, lib, ... }:
+    {
+      # NixOps deployment info
+      deployment.targetHost = ip;
+
+      # Hardware configuration
+      boot.loader.grub.enable = true;
+      boot.loader.grub.version = 2;
+      boot.loader.grub.device = "/dev/sda";
+
+      networking.hostName = name;
+      networking.interfaces.enp2s0.useDHCP = true;
+
+      boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "sd_mod" ];
+      boot.kernelModules = [ "kvm-intel" "nct6775" "coretemp" ];
+
+      fileSystems."/" = {
+        device = "/dev/disk/by-label/nixos";
+        fsType = "ext4";
+      };
+
+      swapDevices = [ { device = "/dev/disk/by-label/swap"; } ];
+
+      powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+    };
+
   # KVM/QEMU Guest Hardware
   kvmGuest =
     { name, ip }:
