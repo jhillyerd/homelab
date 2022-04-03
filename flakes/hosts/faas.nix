29f2839e7191d54b4afb9c5931f4721beed8a4f1
James Hillyerd <james@hillyerd.com>
2022-04-03 15:18:24 -0700

    faas: use password for admin

--- flakes/hosts/faas.nix
+++ flakes/hosts/faas.nix
@@ -1,9 +1,19 @@
-{ pkgs, faasd, ... }: {
+{ pkgs, faasd, ... }:
+let
+  # Import low security credentials.
+  lowsec = import ./../lowsec.nix;
+in {
   imports = [ faasd.nixosModules.faasd ];
 
-  services.faasd = {
+  services.faasd = let
+    passwordFile = pkgs.writeText "faasd-basic-auth" lowsec.faasd.admin.password;
+  in {
     enable = true;
-    basicAuth.enable = false;
+    basicAuth = {
+      enable = true;
+      user = lowsec.faasd.admin.user;
+      passwordFile = "${passwordFile}";
+    };
   };
 
   networking.firewall.allowedTCPPorts = [ 8080 ];
