a98505d2cb534faf47aed6f91964c2c912d6ca2c
James Hillyerd <james@hillyerd.com>
2022-04-08 19:33:55 -0700

    import authorized_keys

--- nixops/common.nix
+++ nixops/common.nix
@@ -1,5 +1,5 @@
 # Common config shared among all machines
-{ pkgs, nodes, hostName, environment, ... }:
+{ pkgs, nodes, hostName, environment, lib, ... }:
 let
   # Import low security credentials.
   lowsec = import ./lowsec.nix;
@@ -33,7 +33,8 @@ in {
     inherit syslogHost syslogPort;
   };
 
-  services.getty.helpLine = ">>> Flake node: ${hostName}, environment: ${environment}";
+  services.getty.helpLine =
+    ">>> Flake node: ${hostName}, environment: ${environment}";
 
   services.tailscale.enable = true;
 
@@ -44,14 +45,16 @@ in {
 
   time.timeZone = "US/Pacific";
 
+  users.users.root.openssh.authorizedKeys.keys =
+    lib.splitString "\n" (builtins.readFile ./authorized_keys.txt);
+
   users.users.james = {
     uid = 1001;
     isNormalUser = true;
     home = "/home/james";
     description = "James Hillyerd";
     extraGroups = [ "docker" "wheel" ];
-    openssh.authorizedKeys.keys = [
-      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDZj6GV5aC3zX/P/STi0QDDaIUCwyDekLIKtN/L+s2vL8E1KxD69DLQ4DKV1fJUV97oo/Qv8pHUFgCQhEOYm5bchm+0Wc6ZBolcJ6q9KUNGIsaIa8ts6vQEG5k3pRI1E4kMrhggUJFOlSKxmcA9v+tEmZTlAo9wXn2wmqhmaLVfaGORwyMCuUc+2BP4xTwfuc+c0rb+kZOdp6+TuYiIXUOD9OqDrBkhFMe9bqNI0QxryACjid/qJvhjMos/fTeg7CgSsp+jP9ChVWnde0QquUVv5jmkKq2cdN2tfZdmin48cvAKAdtibpi4jQcIeWM7xWfEoE9T1u5tkfQgM8VhiV5EmSQrO/U9PIucKh64Vu+PGvQtbeNUcODd5Zkky0NDK2vrnIZTnGwQcTw4j5nDDUkgBHeW8jxT3Pf9lsCtJJL3edLxKwZA2+Dgf6EX2LxovvZVKYgfONhH1FRtv4V9ahoCPg0l1qdYX996Iihwc9wv8DfXMnWypEcytpKP2sXhUqc= james@Ryzen"
-    ];
+    openssh.authorizedKeys.keys =
+      lib.splitString "\n" (builtins.readFile ./authorized_keys.txt);
   };
 }
