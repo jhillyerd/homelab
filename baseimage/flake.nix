6eb4e0dab3b6be15d591a9ecc77cc66a505cd922
James Hillyerd <james@hillyerd.com>
2022-04-10 12:44:40 -0700

    Add a baseimage generator flake

---
+++ baseimage/flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "VM deployment target base images";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
+
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, nixos-generators }:
+    let
+      inherit (nixpkgs) lib;
+      baseModule = { ... }: {
+        services.openssh = {
+          enable = true;
+          permitRootLogin = "yes";
+        };
+
+        time.timeZone = "US/Pacific";
+
+        users.users.root.openssh.authorizedKeys.keys =
+          lib.splitString "\n" (builtins.readFile ../authorized_keys.txt);
+      };
+    in {
+      packages."x86_64-linux" = {
+        hyperv = nixos-generators.nixosGenerate {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          modules = [ baseModule ];
+          format = "hyperv";
+        };
+      };
+    };
+}
