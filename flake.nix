93772c3f1bc40b9c03e8df33c34221f8d2ce99b0
James Hillyerd <james@hillyerd.com>
2022-05-19 20:37:25 -0700

    flake: add agenix

--- flake.nix
+++ flake.nix
@@ -4,11 +4,14 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
 
+    agenix.url = "github:ryantm/agenix";
+    agenix.inputs.nixpkgs.follows = "nixpkgs";
+
     flake-utils.url = "github:numtide/flake-utils";
     flake-utils.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, agenix }:
     flake-utils.lib.eachDefaultSystem
       (system: let
         pkgs = nixpkgs.legacyPackages.${system};
@@ -16,6 +19,7 @@
         {
           devShell = pkgs.mkShell {
             buildInputs = with pkgs; [
+              agenix.defaultPackage.${system}
               ansible_2_9
               consul
               esphome
