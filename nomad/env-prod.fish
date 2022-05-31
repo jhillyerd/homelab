437c0b38e50d1bf312b2f25ae668a3db8fc7b7db
James Hillyerd <james@hillyerd.com>
2022-05-30 20:45:19 -0700

    add nomad.service.consul as cert CN

--- nomad/env-prod.fish
+++ nomad/env-prod.fish
@@ -1,4 +1,4 @@
-set -x NOMAD_ADDR https://nexus.home.arpa:4646
+set -x NOMAD_ADDR https://nomad.service.consul:4646
 set -x NOMAD_CACERT ~/secrets/nomad/ca/nomad-ca.pem
 set -x NOMAD_CLIENT_CERT ~/secrets/nomad/cli.pem
 set -x NOMAD_CLIENT_KEY ~/secrets/nomad/cli-key.pem
--- nomad/etc/make-certs.fish
+++ nomad/etc/make-certs.fish
@@ -9,10 +9,10 @@ function gencert
   set host_names \
     (string join "," (string replace -r '$' ".global.nomad" $argv))
 
-  echo '{}' | cfssl gencert \
-    -ca=$ca_crt -ca-key=$ca_key -config=$config \
-    -hostname="$host_names,$local_wildcard,localhost,127.0.0.1" - \
-    | cfssljson -bare $output/$short_name
+echo '{ "CN": "nomad.service.consul" }' | cfssl gencert \
+  -ca=$ca_crt -ca-key=$ca_key -config=$config \
+  -hostname="$host_names,$local_wildcard,nomad.service.consul,localhost,127.0.0.1" - \
+  | cfssljson -bare $output/$short_name
 end
 
 mkdir -p $output
@@ -22,9 +22,8 @@ gencert server client
 echo '{}' | cfssl gencert -ca=$ca_crt -ca-key=$ca_key -profile=client - \
   | cfssljson -bare $output/cli
 
-echo '{
-  "CN": "nomad.browser"
-}' | cfssl gencert -ca=$ca_crt -ca-key=$ca_key -profile=browser - \
+echo '{ "CN": "nomad.browser" }' | cfssl gencert \
+  -ca=$ca_crt -ca-key=$ca_key -profile=browser - \
   | cfssljson -bare $output/browser
 openssl pkcs12 -export -out $output/browser.pfx -passout pass: \
   -in $ca_crt -in $output/browser.pem -inkey $output/browser-key.pem
