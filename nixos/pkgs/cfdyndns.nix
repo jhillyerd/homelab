51b985f356d2f9cca81aa6a13861e9806f825073
James Hillyerd <james@hillyerd.com>
2022-12-24 14:10:23 -0800

    Use sysr-q distro of cfdyndns, previous stopped working

--- nixos/pkgs/cfdyndns.nix
+++ nixos/pkgs/cfdyndns.nix
@@ -1,19 +1,19 @@
 final: prev: {
   cfdyndns = prev.cfdyndns.overrideAttrs (oldAttrs: rec {
     pname = "cfdyndns";
-    version = "0.3";
+    version = "0.0.3";
 
     src = prev.fetchFromGitHub {
-      owner = "colemickens";
+      owner = "sysr-q";
       repo = "cfdyndns";
-      rev = "dfd6200bedc27ade55b24f396a773201c625de92";
-      sha256 = "sha256-4f26oQIFnfyVUbahl476xq2vJ5DcFZ8AWetqBm2kDHE=";
+      rev = "4e703506df0298423a79be3e0efec7ecf6ae8680";
+      sha256 = "0plijgr5y58ir9mjvxgm6jszz90pd1g0qjf21z0v5xrzg6bs2sy1";
     };
 
     cargoDeps = oldAttrs.cargoDeps.overrideAttrs (prev.lib.const {
       inherit src;
       name = "${pname}-vendor.tar.gz";
-      outputHash = "sha256-JQIljUx6J2Ru/OAJsB77u/qTyk2TR2vTgJSltNmPHpg=";
+      outputHash = "YjOTtkffQRLX5qYiVYcZ8+Fh39ZNIIeA/0W5lnZuwfo=";
     });
   });
 }
