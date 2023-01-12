9925596a3adbff489a96755c663ceaf4b11023be
James Hillyerd <james@hillyerd.com>
2023-01-11 21:27:41 -0800

    Rekey for eph, delete some keys

--- nixos/secrets/secrets.nix
+++ nixos/secrets/secrets.nix
@@ -1,25 +1,25 @@
 let
+  # Users
+  james-eph =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM1Rq9OFHVus5eULteCEGNkHgINch40oPP2LwvlVd6ng";
   james-fractal =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB5SIs0HmrtQN+W7YFqIPpyTqTbRqW8Kq06h2btmXElG";
-  james-hypernix =
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBIYwEkZGwJbEcZve0qLyTbfuD7eKmtafA8ZWGgYHWUW";
   james-ryzen =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICAXHtE9NI16ZPNSKF6Cn0JNJS6fTNQYduerVmVa6WKY";
-  users = [ james-fractal james-hypernix james-ryzen ];
+  users = [ james-eph james-fractal james-ryzen ];
 
+  # Nodes
   eph =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJy9svdsaaW77i5QPwiNLDSN+jJmFvkK5NisZzbm7Lro";
   fractal =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDdjavkGpkN4niiLWGAjNsaS3R3gxnLn7H4rTtkVkyAt";
   nexus =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEuqgUtpyOVfnxa1oKLdjN4AIN5piKHfdumQHonqjH4P";
-  nixtarget1-hyperv =
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILiU0B1BYTeGojG+uo2siv2tpl7PEj3iV0CL8EHyM42e";
   nixtarget1-virtd =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILozTQNcPY2BNQZNW+F29M2euRzD7wZ1XtsKsWFjzpeJ";
   web =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICHzyS01Xs/BFkkwlIa+F3K/23yw/9GE/NFcachriRgl";
-  home-systems = [ eph fractal nexus nixtarget1-hyperv nixtarget1-virtd web ];
+  home-systems = [ eph fractal nexus nixtarget1-virtd web ];
 
   nc-um350-1 =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMY7Sz0qZCTg2rJNZ1SX61eMosZwPyh0Mq8+kxp5AB31";
