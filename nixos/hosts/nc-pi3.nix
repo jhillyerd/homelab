ea5e45de9177e3d4a8631e58d061c8468e9fcc8f
James Hillyerd <james@hillyerd.com>
2023-02-19 17:24:23 -0800

    nc-pi3: add nomad client config

--- nixos/hosts/nc-pi3.nix
+++ nixos/hosts/nc-pi3.nix
@@ -8,6 +8,23 @@
     retryJoin = catalog.consul.servers;
   };
 
+  roles.nomad = {
+    enableClient = true;
+
+    retryJoin = catalog.nomad.servers;
+
+    hostVolumes = lib.genAttrs catalog.nomad.skynas-host-volumes
+      (name: {
+        path = "/mnt/skynas/${name}";
+        readOnly = false;
+      }) // {
+      "docker-sock-ro" = {
+        path = "/var/run/docker.sock";
+        readOnly = true;
+      };
+    };
+  };
+
   roles.gateway-online.addr = "192.168.1.1";
 
   networking.firewall.enable = false;
