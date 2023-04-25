0c1c3ebcd002c2202e9d03842e0312477bd95137
James Hillyerd <james@hillyerd.com>
2023-04-25 09:40:28 -0700

    nexus: update pve1 ping to pve3

--- nixos/hosts/nexus.nix
+++ nixos/hosts/nexus.nix
@@ -113,8 +113,8 @@
       "nc-um350-1.home.arpa"
       "nc-um350-2.home.arpa"
       "octopi.home.arpa"
-      "pve1.home.arpa"
       "pve2.home.arpa"
+      "pve3.home.arpa"
       "skynas.home.arpa"
       "web.home.arpa"
     ];
