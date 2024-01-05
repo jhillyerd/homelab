43d749b7074afe1a05cfa8c439029e8b6ca91c10
James Hillyerd <james@hillyerd.com>
2024-01-04 22:06:37 -0800

    nexus: remove mosquitto role

--- nixos/hosts/nexus.nix
+++ nixos/hosts/nexus.nix
@@ -4,29 +4,6 @@
   roles.dns.bind.enable = true;
   roles.dns.bind.serveLocalZones = true;
 
-  roles.mosquitto = {
-    enable = true;
-
-    users = {
-      admin = {
-        passwordFile = config.age.secrets.mqtt-admin.path;
-        acl = [ "readwrite $SYS/#" "readwrite #" ];
-      };
-      clock = {
-        passwordFile = config.age.secrets.mqtt-clock.path;
-        acl = [ "readwrite clock/#" ];
-      };
-      sensor = {
-        passwordFile = config.age.secrets.mqtt-sensor.path;
-        acl = [ ];
-      };
-      zwave = {
-        passwordFile = config.age.secrets.mqtt-zwave.path;
-        acl = [ "readwrite zwave/#" ];
-      };
-    };
-  };
-
   roles.consul = {
     enableServer = true;
     retryJoin = catalog.consul.servers;
@@ -40,20 +17,6 @@
   roles.gateway-online.addr = "192.168.1.1";
 
   roles.tailscale.exitNode = true;
-
-  age.secrets = {
-    mqtt-admin.file = ../secrets/mqtt-admin.age;
-    mqtt-admin.owner = "mosquitto";
-
-    mqtt-clock.file = ../secrets/mqtt-clock.age;
-    mqtt-clock.owner = "mosquitto";
-
-    mqtt-sensor.file = ../secrets/mqtt-sensor.age;
-    mqtt-sensor.owner = "mosquitto";
-
-    mqtt-zwave.file = ../secrets/mqtt-zwave.age;
-    mqtt-zwave.owner = "mosquitto";
-  };
 
   networking.firewall.enable = false;
 }
