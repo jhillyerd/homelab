431d0a306d6c91ba6c7c63a3c6f2aeccf6e5018c
James Hillyerd <james@hillyerd.com>
2024-01-18 10:10:32 -0800

    proxmox: trim unnecessary kernel modules
    Signed-off-by: James Hillyerd <james@hillyerd.com>

--- nixos/hw/proxmox.nix
+++ nixos/hw/proxmox.nix
@@ -9,11 +9,13 @@
   # Hardware configuration
   imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
 
-  boot.initrd.availableKernelModules =
-    [ "ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ ];
-  boot.extraModulePackages = [ ];
+  boot = {
+    initrd.availableKernelModules = [ "uas" ];
+    initrd.kernelModules = [ ];
+
+    kernelModules = [ ];
+    extraModulePackages = [ ];
+  };
 
   fileSystems."/" = {
     device = "/dev/disk/by-label/nixos";
