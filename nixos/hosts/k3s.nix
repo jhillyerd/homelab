c10c51728dfdb3f72d72fa668a568b432458708c
James Hillyerd <james@hillyerd.com>
2024-01-24 11:34:40 -0800

    k3s: nfs support
    Signed-off-by: James Hillyerd <james@hillyerd.com>

--- nixos/hosts/k3s.nix
+++ nixos/hosts/k3s.nix
@@ -6,6 +6,10 @@
   systemd.network.networks = util.mkClusterNetworks self;
   roles.gateway-online.addr = "192.168.1.1";
 
+  # NFS mount support
+  boot.supportedFilesystems = [ "nfs" ];
+  services.rpcbind.enable = true;
+
   services.k3s =
     let
       leader = catalog.k3s.leader;
