0bc12ace9394eaccbee2bdce3bacb11ba0c616b8
James Hillyerd <james@hillyerd.com>
2024-01-28 11:49:35 -0800

    flake.nix: tidy
    Signed-off-by: James Hillyerd <james@hillyerd.com>

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -4,54 +4,53 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
 
     agenix.url = "github:ryantm/agenix/0.13.0";
-    agenix.inputs.nixpkgs.follows = "nixpkgs";
+    agenix.inputs = {
+      nixpkgs.follows = "nixpkgs";
+    };
 
     devenv.url = "github:cachix/devenv/v0.6";
 
     homesite.url = "github:jhillyerd/homesite/main";
-    homesite.inputs.flake-utils.follows = "flake-utils";
-    homesite.inputs.nixpkgs.follows = "nixpkgs";
+    homesite.inputs = {
+      flake-utils.follows = "flake-utils";
+      nixpkgs.follows = "nixpkgs";
+    };
 
     hw-gauge.url = "github:jhillyerd/hw-gauge";
-    hw-gauge.inputs.flake-utils.follows = "flake-utils";
-    hw-gauge.inputs.nixpkgs.follows = "nixpkgs";
-
-    flake-utils.url = "github:numtide/flake-utils";
+    hw-gauge.inputs = {
+      flake-utils.follows = "flake-utils";
+      nixpkgs.follows = "nixpkgs";
+    };
 
     terranix.url = "github:terranix/terranix";
-    terranix.inputs.flake-utils.follows = "flake-utils";
-    terranix.inputs.nixpkgs.follows = "nixpkgs";
+    terranix.inputs = {
+      flake-utils.follows = "flake-utils";
+      nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    { self
-    , nixpkgs
-    , nixpkgs-unstable
-    , flake-utils
-    , agenix
-    , ...
-    }@inputs:
+    { self, nixpkgs, flake-utils, ... }@inputs:
     let
-      inherit (nixpkgs.lib)
-        mapAttrs mapAttrs' mapAttrsToList mkMerge nixosSystem splitString;
-
+      inherit (nixpkgs.lib) mapAttrs;
       inherit (flake-utils.lib) eachSystemMap system;
 
       # catalog.nodes defines the systems available in this flake.
       catalog = import ./catalog { inherit system; };
     in
     rec {
-      # Convert nodes into a set of nixos configs.
+      # Convert catalog.nodes into a set of NixOS configs.
       nixosConfigurations = import ./nix/nixos-configurations.nix inputs catalog;
 
-      # Generate an SD card image for each host.
+      # Generate an SD card image for each node in the catalog.
       images = mapAttrs
         (host: node: nixosConfigurations.${host}.config.system.build.sdImage)
         catalog.nodes;
 
-      # Terraform commands.
+      # Homelab commands.
       apps =
         let
           tf-apps = import ./terraform/apps.nix inputs catalog;
