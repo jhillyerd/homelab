c8a7d9341d5a09bfb2e3539e1c20d05373eeb561
James Hillyerd <james@hillyerd.com>
2024-01-27 16:31:39 -0800

    tailscale: use built-in authKeyFile support
    Signed-off-by: James Hillyerd <james@hillyerd.com>

--- nixos/roles/tailscale.nix
+++ nixos/roles/tailscale.nix
@@ -20,36 +20,9 @@ in
     services.tailscale = {
       enable = true;
       interfaceName = catalog.tailscale.interface;
-    };
 
-    # Create a oneshot job to authenticate to Tailscale.
-    systemd.services.tailscale-autoconnect = mkIf cfg.useAuthKey {
-      description = "Automatic connection to Tailscale";
-
-      # Make sure tailscale is running before trying to connect.
-      after = [ "network-pre.target" "tailscale.service" ];
-      wants = [ "network-pre.target" "tailscale.service" ];
-      wantedBy = [ "multi-user.target" ];
-
-      serviceConfig.Type = "oneshot";
-
-      script = with pkgs;
-        let
-          exitNode = if cfg.exitNode then "--advertise-exit-node" else "";
-        in
-        ''
-          ts=${tailscale}/bin/tailscale
-
-          # Wait for tailscaled to settle.
-          sleep 2
-
-          if $ts status --peers=false >/dev/null; then
-            # Already online, do nothing.
-            exit 0
-          fi
-
-          $ts up -authkey "$(< ${config.age.secrets.tailscale.path})" ${exitNode}
-        '';
+      authKeyFile = mkIf cfg.useAuthKey config.age.secrets.tailscale.path;
+      extraUpFlags = mkIf cfg.exitNode [ "--advertise-exit-node" ];
     };
 
     age.secrets.tailscale.file = mkIf cfg.useAuthKey ../secrets/tailscale.age;
