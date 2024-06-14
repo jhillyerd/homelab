0acbc16a8b4952e44baf8594cf26a96c83273964
James Hillyerd <james@hillyerd.com>
2024-06-14 16:23:15 -0700

    carbon: backlight keys
    Signed-off-by: James Hillyerd <james@hillyerd.com>

--- nixos/hosts/carbon.nix
+++ nixos/hosts/carbon.nix
@@ -9,6 +9,16 @@
   networking.networkmanager.enable = true;
   networking.firewall.enable = false;
 
+  programs.light.enable = true;
+
+  services.actkbd = {
+    enable = true;
+    bindings = [
+      { keys = [ 224 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -U 10"; }
+      { keys = [ 225 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -A 10"; }
+    ];
+  };
+
   virtualisation.libvirtd.enable = true;
 
   system.stateVersion = lib.mkOverride 0 "24.05";
