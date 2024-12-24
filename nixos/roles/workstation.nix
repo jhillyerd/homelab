845a38e264117ce6807a2cdeeca5482c8076a51b
James Hillyerd <james@hillyerd.com>
2024-12-24 10:34:37 -0800

    extend pipewire config
    Signed-off-by: James Hillyerd <james@hillyerd.com>

--- nixos/roles/workstation.nix
+++ nixos/roles/workstation.nix
@@ -231,7 +231,13 @@ in
       ];
 
       # Enable sound.
-      services.pipewire.pulse.enable = true;
+      security.rtkit.enable = true;
+      services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+      };
 
       hardware.bluetooth.enable = true;
       hardware.bluetooth.powerOnBoot = true;
