26f7d760fdad71e6cc49f2b466f35581dfe6030f
James Hillyerd <james@hillyerd.com>
2025-05-10 11:10:30 -0700

    wayland: enable chromium OZONE

--- nixos/roles/gui-wayland.nix
+++ nixos/roles/gui-wayland.nix
@@ -45,6 +45,9 @@ in
       ])
       ++ remaps;
 
+    # Enable Ozone Wayland support in Chromium and Electron based applications
+    environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
     programs.dconf.enable = true;
 
     programs.thunar = {
