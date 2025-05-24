cf3741e8dbfae7ae48fccea644ce91eeb1204aa9
James Hillyerd <james@hillyerd.com>
2025-05-24 15:50:48 -0700

    nixos to 25.05, nomad to 1.9.x
    Signed-off-by: James Hillyerd <james@hillyerd.com>

--- nixos/roles/nomad.nix
+++ nixos/roles/nomad.nix
@@ -120,7 +120,7 @@ in
       # Nomad shared client & server config.
       services.nomad = {
         enable = true;
-        package = pkgs.nomad_1_8;
+        package = pkgs.nomad_1_9;
 
         dropPrivileges = false;
 
