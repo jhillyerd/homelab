102e51a32e641ad3a55473a883546451afa331da
James Hillyerd <james@hillyerd.com>
2026-01-01 10:04:25 -0800

    bump outer flake
    Signed-off-by: James Hillyerd <james@hillyerd.com>

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "my nixos & ansible configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     agenix.url = "github:ryantm/agenix/main";
