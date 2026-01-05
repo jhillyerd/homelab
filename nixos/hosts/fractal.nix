1af3f1467dbcf831e3789ec5a1232998a0e34e36
James Hillyerd <james@hillyerd.com>
2026-01-04 19:56:47 -0800

    fractal: devstral2 too picky, trying out qwen3
    Signed-off-by: James Hillyerd <james@hillyerd.com>

--- nixos/hosts/fractal.nix
+++ nixos/hosts/fractal.nix
@@ -38,15 +38,18 @@
             "/data/llama/models:/models"
           ];
           environment = {
-            LLAMA_ARG_CTX_SIZE = "49152";
+            LLAMA_ARG_CTX_SIZE = "65536";
             LLAMA_ARG_JINJA = "true";
-            LLAMA_ARG_TEMP = "0.15";
-            LLAMA_ARG_MIN_P = "0.01";
+            LLAMA_ARG_TEMP = "0.7";
+            LLAMA_ARG_TOP_K = "20";
+            LLAMA_ARG_MIN_P = "0.0";
+            LLAMA_ARG_TOP_P = "0.8";
+            LLAMA_ARG_REPEAT_PENALTY = "1.05";
             LLAMA_ARG_GPU_LAYERS = "99";
           };
           cmd = [
             "-hf"
-            "unsloth/Devstral-Small-2-24B-Instruct-2512-GGUF:IQ4_NL"
+            "unsloth/Qwen3-Coder-30B-A3B-Instruct-GGUF:IQ4_NL"
           ];
         };
       };
