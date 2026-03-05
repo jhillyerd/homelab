e1eb0391371634306e38db2c37c9cb2afb65e4f2
James Hillyerd <james@hillyerd.com>
2026-03-05 13:49:52 -0800

    fractal: tweak Qwen3.5 settings
    Signed-off-by: James Hillyerd <james@hillyerd.com>

--- nixos/hosts/fractal.nix
+++ nixos/hosts/fractal.nix
@@ -51,11 +51,14 @@
           ports = [ "8001:8080" ]; # healthcheck runs against 8080.
           environment = {
             LLAMA_ARG_CTX_SIZE = "131072";
+            LLAMA_ARG_UBATCH = "1024"; # Faster PP, but more VRAM usage
+            LLAMA_ARG_IMAGE_MIN_TOKENS = "1024"; # Improves small image results
             LLAMA_ARG_TEMP = "0.6";
             LLAMA_ARG_MIN_P = "0.0";
             LLAMA_ARG_TOP_P = "0.95";
             LLAMA_ARG_TOP_K = "20";
             # LLAMA_ARG_REPEAT_PENALTY
+            LLAMA_ARG_THINK_BUDGET = "0";
             LLAMA_ARG_GPU_LAYERS = "all";
           };
           cmd = [
