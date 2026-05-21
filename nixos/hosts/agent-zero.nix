43b2a386fc208c341f41136c2552d89a9bfe8c18
James Hillyerd <james@hillyerd.com>
2026-05-21 08:54:32 -0700

    a0: bump
    Signed-off-by: James Hillyerd <james@hillyerd.com>

--- nixos/hosts/agent-zero.nix
+++ nixos/hosts/agent-zero.nix
@@ -12,7 +12,7 @@
   virtualisation.oci-containers = {
     containers = {
       agent0 = {
-        image = "agent0ai/agent-zero:v1.11";
+        image = "agent0ai/agent-zero:v1.15";
         hostname = "agent0";
         ports = [
           "80:80/tcp"
